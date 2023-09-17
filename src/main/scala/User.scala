082fad8b2ca5f5f38813ba6848a452c43124acdd
CHEESE <81912261+ychxexn@users.noreply.github.com>
2023-09-17 20:17:17 +0900

    feat: add file manager

--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -1,2 +1,9 @@
+import zio.json.{DeriveJsonDecoder, DeriveJsonEncoder, JsonDecoder, JsonEncoder}
+
 case class User(name: String,
                 phone: String)
+
+object User {
+  implicit val decoder: JsonDecoder[User] = DeriveJsonDecoder.gen[User]
+  implicit val encoder: JsonEncoder[User] = DeriveJsonEncoder.gen[User]
+}
