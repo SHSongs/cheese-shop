fc1ba6ec54986e7a578507c93a206dad2b541428
CHEESE <ychxexn@gmail.com>
2023-09-18 21:34:30 +0900

    FileManager 작성 (#12)
    * chore: .gitignore에 resources 경로 추가
    
    * modify: fileManager 저장 방식 변경
    
    * chore: 오탈자 수정
    
    * chore: resources 디렉토리 내 파일 삭제
    
    ---------
    
    Co-authored-by: CHEESE <81912261+ychxexn@users.noreply.github.com>

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,12 @@
 import file.FileManager
 import zio._
+import zio.json._
 
 import java.io.IOException
 
 object Main extends ZIOAppDefault {
+  val user = User("익명", "010-1111-2222")
+
   val inputType: ZIO[Any, IOException, String] = for {
     _ <- zio.Console.printLine("1. 사장님")
     _ <- zio.Console.printLine("2. 손님")
@@ -17,8 +20,8 @@ object Main extends ZIOAppDefault {
     _ <- userType match {
       case "1" => OwnerService.run()
       case "2" => UserService.run()
-      case "3" => FileManager.readJson("example", "hello.txt").debug("result => ")
-      case "4" => FileManager.writeJson("example", "input.txt", User("익명", "010-1111-2222").toString).debug("result => ")
+      case "3" => FileManager.readJson("user.json").debug("result => ")
+      case "4" => FileManager.writeJson("user.json", user.toJson).debug("result => ")
       case _ => zio.Console.printLine("잘못된 입력입니다. 프로그램을 종료합니다.")
     }
   } yield ()
--- src/main/scala/file/FileManager.scala
+++ src/main/scala/file/FileManager.scala
@@ -1,19 +1,28 @@
 package file
 
 import zio._
+import ujson._
 
 object FileManager {
   val path = os.pwd / "src" / "resources"
 
-  def readJson(directoryName: String, fileName: String) = for {
+  def readJson(fileName: String) = for {
     json <- ZIO
-      .attempt(ujson.read(os.read(path / directoryName / fileName)))
+      .attempt(ujson.read(os.read(path / fileName)).obj.get("data").get)
       .catchAll(cause => ZIO.fail(s"Read Fail: ${cause}"))
   } yield json
 
-  def writeJson(directoryName: String, fileName: String, contents: String) = for {
+  def writeJson(fileName: String, contents: Value) = for {
+    _ <- ZIO.unit
+    filePath = path / fileName
+    existingContents <- readJson(fileName).orElse(ZIO.succeed(Arr()))
+    updatedContents: Arr = existingContents match {
+      case arr: Arr => arr.arr :+ contents
+      case _ => Arr(contents)
+    }
+    jsonContents = ujson.Obj("data" -> updatedContents)
     _ <- ZIO
-      .attempt(os.write(path / directoryName / fileName, contents))
+      .attempt(os.write.over(filePath, jsonContents))
       .catchAll(cause => ZIO.fail(s"Write Fail: ${cause}"))
   } yield ()
 }
