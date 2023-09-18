594e680e666d452ca545ce6f5bdab8d1fb9954df
Adelie-penguin <91370858+darkenpeng@users.noreply.github.com>
2023-09-18 14:04:24 +0900

    Revert "파일 입출력을 위한 공통 코드 추가"

--- src/main/scala/User.scala
+++ src/main/scala/User.scala
@@ -1,8 +1,2 @@
-import zio.json.{DeriveJsonCodec, JsonCodec}
-
 case class User(name: String,
                 phone: String)
-
-object User {
-  implicit val codec: JsonCodec[User] = DeriveJsonCodec.gen[User]
-}
