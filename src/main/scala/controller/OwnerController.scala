64f7255d2dce379cd4c5bcd037c8ee35b89a8d1d
CHEESE <81912261+ychxexn@users.noreply.github.com>
2023-09-19 21:04:08 +0900

    feat(Owner): #15 예약 종료 추가

--- src/main/scala/controller/OwnerController.scala
+++ src/main/scala/controller/OwnerController.scala
@@ -14,5 +14,10 @@ object OwnerController {
           reservations <- OwnerService.getReservations()
           res <- ZIO.succeed(Response.json(reservations.toJson))
         } yield res
+      case Method.PUT -> Root / "owner" / "reservations" / reservationId => for {
+        _ <- zio.Console.printLine(s"/owner/reservations/${reservationId} endpoint!")
+        data <- OwnerService.closeReservation(Integer.parseInt(reservationId))
+        res <- ZIO.succeed(Response.json(data.toJson))
+      } yield res
     }
 }
--- src/main/scala/service/OwnerService.scala
+++ src/main/scala/service/OwnerService.scala
@@ -4,18 +4,7 @@ import file.FileManager
 import model.Reservation
 
 object OwnerService {
-  def run() = userProg()
-
-  private def userProg() = for {
-    _ <- zio.Console.printLine("사장님 프로그램입니다.")
-    menu <- inputMenu()
-    _ <- menu match {
-      case "1" => readReservation()
-      case "2" => close()
-      case "3" => readReview()
-      case _ => zio.Console.printLine("잘못된 입력입니다. 프로그램을 종료합니다.")
-    }
-  } yield ()
+  private val FILE_RESERVATION = "reservation.json"
 
   private def inputMenu() = for {
     _ <- zio.Console.printLine("1. 예약 조회")
@@ -24,39 +13,20 @@ object OwnerService {
     choice <- zio.Console.readLine("입력: ")
   } yield choice
 
-  private def readReservation() = for {
-    _ <- printReservations()
-  } yield ()
-
-  private def close() = for {
-    _ <- printReservations()
-    _ <- closeReservation()
-  } yield ()
-
-  private def readReview() = for {
-    _ <- zio.Console.printLine("전체 리뷰 목록을 가져옵니다...")
-  } yield ()
-
   def getReservations() = for {
-    reservations <- FileManager.readJson[Reservation]("reservation.json")
-    _ <- zio.Console.printLine(reservations)
+    reservations <- FileManager.readJson[Reservation](FILE_RESERVATION)
   } yield reservations
 
-  private def printReservations() = for {
+  def closeReservation(reservationId: Int) = for {
     reservations <- getReservations()
-    _ <- zio.Console.printLine(reservations)
-  } yield ()
-
-  private def closeReservation() = for {
-    reservations <- getReservations()
-    target <- zio.Console.readLine("종료할 예약을 선택하세요 : ")
-    changed = reservations.map { reservation =>
-      if (Integer.parseInt(target) == reservation.id) {
+    changedList = reservations.map { reservation =>
+      if (reservationId == reservation.id) {
         reservation.close()
       } else {
         reservation
       }
     }
-    _ <- FileManager.writeJson("reservation.json", changed)
-  } yield ()
+    _ <- FileManager.writeJson(FILE_RESERVATION, changedList)
+    changedReservation = changedList.filter(r => r.id == reservationId)
+  } yield changedReservation.head
 }
