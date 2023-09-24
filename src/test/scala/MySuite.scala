99e7a6e648b60c44d36efdbb4aae0c4587664af1
chloeseo0210 <chloeseo0210@gmail.com>
2023-09-24 22:40:16 +0900

    feat: Owner E2E test

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,14 +1,34 @@
+import model.{Reservation, Review}
 import zio.test._
 import zio.http.{Root, _}
+import zio.json.{DecoderOps, EncoderOps, JsonEncoder}
 
 object MySuite extends ZIOSpecDefault {
   def spec = suite("MySuite")(
     test("root로 요청을 보내면 Hello World! 를 body로 응답한다") {
-      val app = Main.app
       for {
-        response <- app.runZIO(Request.get(URL(Root)))
+        response <- Main.helloApp.runZIO(Request.get(URL(Root)))
         result <- response.body.asString
       } yield assertTrue(result == "Hello World!")
+    },
+    test("사장님은 모든 예약 목록을 확인할 수 있다") {
+      for {
+        response <- Main.apps.runZIO(Request.get(URL(Root / "owner" / "reservations")))
+        result <- response.body.asString.map(_.fromJson[List[Reservation]])
+      } yield assertTrue(result.isRight)
+    },
+    test("사장님은 모든 리뷰 목록을 확인할 수 있다") {
+      for {
+        response <- Main.apps.runZIO(Request.get(URL(Root / "owner" / "reviews")))
+        result <- response.body.asString.map(_.fromJson[List[Review]])
+      } yield assertTrue(result.isRight)
+    },
+    test("사장님은 예약을 종료할 수 있다.") {
+      for {
+        response <- Main.apps.runZIO(Request.put(Body.empty, URL(Root / "owner" / "reservations" / "1")))
+        _ = assertTrue(response.status == Status.Ok)
+        result <- response.body.asString.map(_.fromJson[Reservation])
+      } yield assertTrue(result.isRight)
     }
   )
 }
