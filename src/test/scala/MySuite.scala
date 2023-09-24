a75854529ff0fa77f978f62b534064dc99ebf698
chloeseo0210 <chloeseo0210@gmail.com>
2023-09-24 21:47:25 +0900

    feat : Hello world Test

--- src/test/scala/MySuite.scala
+++ src/test/scala/MySuite.scala
@@ -1,6 +1,14 @@
 import zio.test._
+import zio.http.{Root, _}
 
 object MySuite extends ZIOSpecDefault {
   def spec = suite("MySuite")(
+    test("root로 요청을 보내면 Hello World! 를 body로 응답한다") {
+      val app = Main.app
+      for {
+        response <- app.runZIO(Request.get(URL(Root)))
+        result <- response.body.asString
+      } yield assertTrue(result == "Hello World!")
+    }
   )
 }
