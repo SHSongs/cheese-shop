8cdf39f08f67382fa91a81660e11479372eca643
chloeseo0210 <chloeseo0210@gmail.com>
2023-10-04 21:33:08 +0900

    feat : userController after conflict
    E2E에 맞춰 타입을 변형했습니다

--- src/main/scala/controller/UserController.scala
+++ src/main/scala/controller/UserController.scala
@@ -1,9 +1,9 @@
 package controller
 
+import DTO.{RegisterReservationDTO, RegisterReviewDTO}
 import zio._
 import zio.http._
-import zio.json.EncoderOps
-
+import zio.json.{DecoderOps, EncoderOps}
 import service.UserService
 import model._
 
@@ -19,39 +19,86 @@ object UserController {
           case false => Right(queryParams)
         }
 
-        user = parseUserData(userData)
+        user <- parseUserData(userData)
 
-        result <- UserService.findReservationsByUser(user)
+        response <- UserService.findReservationsByUser(user)
 
-        response <- result match {
-          case Left(error)  => ZIO.succeed(BadRequestResponse(error))
-          case Right(value) => ZIO.succeed(Response.json(value.toJson))
-        }
+          .map(r => Response.json(r.toJson))
+          .catchAll(e => ZIO.succeed(BadRequestResponse(e)))
 
       } yield response
-  }
 
-  private def parseUserData(userData: Either[String, QueryParams]) = for {
-    result <- userData match {
-      case Left(error) => Left(error)
+    // 손님은 이름, 휴대폰번호, 예약일시, 인원수를 입력하여 치즈식당을 예약할 수 있다.
+    case req @ (Method.POST -> Root / "user" / "register" / "review") =>
+      for {
+        _ <- Console.printLine("/user/register review endpoint!")
+
+        // request body 파싱
+        reservation <- req.body.asString.map(_.fromJson[RegisterReservationDTO])
+
+        // reservation 만들기
+        result = ZIO
+          .fromEither(reservation)
+          .flatMap(r =>
+            UserService.makeReservation(r.user, r.date, r.time, r.guestCount)
+          )
+
+        // TODO: POST 요청에 응답하는 것에 대한 협의 필요
+        response <- result
+          .map(r => Response.text(s"reservation created: ${r.id}"))
+          .catchAll(e => ZIO.succeed(BadRequestResponse(e)))
+
+      } yield response
+
+    // 손님은 자신의 종료된 예약을 결제할 수 있다.
+    case req @ (Method.PATCH -> Root / "user" / "pay" / "reservation" / reservationId) =>
+      for {
+        _ <- Console.printLine("/user/pay reservation endpoint!")
+        reservation = UserService.pay(reservationId)
+
+        response <- reservation
+          .map(r => Response.text(s"예약이 결제되었습니다.${r.id}"))
+          .catchAll(e => ZIO.succeed(BadRequestResponse(s"결제에 실패했습니다. : ${e}")))
+
+      } yield response
+
+    // 손님은 자신의 결제된 예약에 리뷰할 수 있다.
+
+    case req @ (Method.POST -> Root / "user" / "review" / reservationId) =>
+      for {
+        _ <- Console.printLine("/user/add review endpoint!")
+
+        review <- req.body.asString.map(_.fromJson[RegisterReviewDTO])
+        result <- ZIO.fromEither(review)
+
+        response <- UserService
+          .writeReview(reservationId, result.point, result.content)
+          .map(r => Response.text(s"리뷰를 작성하였습니다.${r.reservationId}"))
+          .catchAll(e =>
+            ZIO.succeed(BadRequestResponse(s"리뷰 작성에 실패했습니다. : ${e}"))
+          )
+      } yield response
+
+  }
+  private def parseUserData(userData: Either[String, QueryParams]) =
+    userData match {
+      case Left(error) => ZIO.fail(error)
       case Right(data) =>
         for {
           name <- parseParam(data, "name")
           phone <- parseParam(data, "phone")
         } yield User(name, phone)
     }
-  } yield result
 
-  private def parseParam(queryParams: QueryParams, parameter: String) = for {
-    result <- queryParams.get(parameter) match {
-      case None => Left(s"다음 정보를 입력해주세요: ${parameter}")
+  private def parseParam(queryParams: QueryParams, parameter: String) =
+    queryParams.get(parameter) match {
+      case None => ZIO.fail(s"다음 정보를 입력해주세요: ${parameter}")
       case Some(param) =>
         param.headOption match {
-          case None        => Left(s"다음 정보를 입력해주세요: ${parameter}")
-          case Some(value) => Right(value)
+          case None        => ZIO.fail(s"다음 정보를 입력해주세요: ${parameter}")
+          case Some(value) => ZIO.succeed(value)
         }
     }
-  } yield result
 
   private def BadRequestResponse(errorMessage: String) =
     Response
